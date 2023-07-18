178a48c10e0281e01aac9f8746deb439f7be2447
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-07-18 09:38:52 -0400

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,17 @@ import Youtube from "./components/Youtube";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col items-center  py-12 px-4 gap-4">
+    <main className="flex w-full min-h-screen flex-col items-center  py-8 px-4 gap-4">
       <Banner text="MAAD 97.5 FM" text2="IS WE OWN" />
-      {/* <BannerImage /> */}
+      <BannerImage />
       <div>
         <p className=" text-xl md:text-4xl text-white font-semibold tracking-wider text-center">
           Welcome to MAAD 97.5 FM, the Maddest online radio station on the
-          internet! We are the home of the freshest tunes, the craziest DJs, and
-          the most unforgettable memories you will ever make.
+          internet!
+          {/* <p className="text-base font-light">
+            We are the home of the freshest tunes, the craziest DJs, and the
+            most unforgettable memories you will ever make.
+          </p> */}
         </p>
       </div>
       <Schedule />
