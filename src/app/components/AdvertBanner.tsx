26a839af727b4cc566d70e5bc2088ff19bdb3d83
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-08-22 07:36:06 -0400

    banner location

--- src/app/components/AdvertBanner.tsx
+++ src/app/components/AdvertBanner.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Image from 'next/image'
+import banneer from '/public/IBC.jpeg'
 
 const AdvertBanner = () => {
   return (
     <div className="w-full pt-8">
-      <Image width={1600} height={200} src="/ibc.jpeg" alt="Advert Banner" layout="responsive" />
+      <Image width={1600} height={200} src={banneer} alt="Advert Banner" layout="responsive" />
     </div>
   )
 }
