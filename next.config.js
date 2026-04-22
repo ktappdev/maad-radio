b3cf06aafa564f9f8873e03b8aca4802d1e6f31c
Ken Taylor <kentaylorappdev@gmail.com>
2026-04-22 12:38:36 -0400

    Add image domains for App Store/Play Store badges

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com", "i.pravatar.cc"],
+    domains: ["source.unsplash.com", "i.pravatar.cc", "upload.wikimedia.org", "developer.apple.com"],
   },
 };
 
