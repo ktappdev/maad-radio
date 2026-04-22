a4f1c073149946f3b16c5caae62748daf4585881
Ken Taylor <kentaylorappdev@gmail.com>
2026-04-22 12:55:51 -0400

    Fix Player audioUrl prop

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -6,7 +6,7 @@ import GetLiveVideo from '../components/GetLiveVideo';
 export default function PlayPage() {
   return (
     <main className="min-h-screen bg-[#111111] flex flex-col items-center justify-center px-4 py-8 gap-6">
-      <Player />
+      <Player audioUrl="https://streaming.broadcastradio.com/brutal" />
       <GetLiveVideo />
     </main>
   );
