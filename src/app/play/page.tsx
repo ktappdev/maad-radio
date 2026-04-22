603b7fc1a24d7260daee32efbce579ba8f6bf35f
Ken Taylor <kentaylorappdev@gmail.com>
2026-04-22 12:46:06 -0400

    Add /play page - player + Facebook embed only

---
+++ src/app/play/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Player from '../components/Player';
+import GetLiveVideo from '../components/GetLiveVideo';
+
+export default function PlayPage() {
+  return (
+    <main className="min-h-screen bg-[#111111] flex flex-col items-center justify-center px-4 py-8 gap-6">
+      <Player />
+      <GetLiveVideo />
+    </main>
+  );
+}
